dd4a855a8643b735e82d2f1cbe217aaa99a9cee9
Asadullah Mumtaz <asad.mumtaz@telenorbank.pk>
2022-03-01 19:27:32 +0500

    first test developed and failing

--- app/src/main/java/com/example/testdrivendevelopment_sample/exampleOne/PingServerSyncUseCase.java
+++ app/src/main/java/com/example/testdrivendevelopment_sample/exampleOne/PingServerSyncUseCase.java
@@ -1,10 +1,23 @@
 package com.example.testdrivendevelopment_sample.exampleOne;
 
 
+import com.example.testdrivendevelopment_sample.exampleOne.networking.PingServerHttpEndpointSync;
+
 public class PingServerSyncUseCase {
 
     public enum UseCaseResult {
         FAILURE,
         SUCCESS
+    }
+
+    private PingServerHttpEndpointSync pingServerHttpEndpointSync;
+
+    PingServerSyncUseCase(PingServerHttpEndpointSync pingServerHttpEndpointSync) {
+        this.pingServerHttpEndpointSync = pingServerHttpEndpointSync;
+
+    }
+
+    public UseCaseResult pingServer() {
+        return null;
     }
 }
--- app/src/test/java/com/example/testdrivendevelopment_sample/exampleOne/PingServerSyncUseCaseTest.java
+++ app/src/test/java/com/example/testdrivendevelopment_sample/exampleOne/PingServerSyncUseCaseTest.java
@@ -1,7 +1,17 @@
 package com.example.testdrivendevelopment_sample.exampleOne;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import com.example.testdrivendevelopment_sample.exampleOne.networking.PingServerHttpEndpointSync;
+
+import org.hamcrest.MatcherAssert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -9,9 +19,30 @@ public class PingServerSyncUseCaseTest {
 
     PingServerSyncUseCase SUT;
 
+    @Mock
+    PingServerHttpEndpointSync pingServerHttpEndpointSyncMock;
+
     @Before
-    public void setup(){
-        SUT = new PingServerSyncUseCase();
+    public void setup() {
+        SUT = new PingServerSyncUseCase(pingServerHttpEndpointSyncMock);
+
+        success();
     }
+
+    @Test
+    public void pingServerSync_success_successReturned() {
+        PingServerSyncUseCase.UseCaseResult result = SUT.pingServer();
+
+        MatcherAssert.assertThat(result, is(PingServerSyncUseCase.UseCaseResult.SUCCESS));
+    }
+
+    @Test
+    public void testing(){
+        assertTrue(true);
+    }
+    private void success() {
+        when(pingServerHttpEndpointSyncMock.pingServerSync()).thenReturn(PingServerHttpEndpointSync.EndpointResult.GENERAL_ERROR);
+    }
+
 
 }
