e58330d04aeafc0d7a9a4e6b2b81089193da1fba
Asadullah Mumtaz <asad.mumtaz@telenorbank.pk>
2022-03-02 18:26:24 +0500

    first test case added i.e failing

--- app/src/main/java/com/example/testdrivendevelopment_sample/exampleTwo/AddToCartUseCaseSync.java
+++ app/src/main/java/com/example/testdrivendevelopment_sample/exampleTwo/AddToCartUseCaseSync.java
@@ -1,9 +1,23 @@
 package com.example.testdrivendevelopment_sample.exampleTwo;
 
+import com.example.testdrivendevelopment_sample.exampleTwo.networking.AddToCartHttpEndpointSync;
+
 public class AddToCartUseCaseSync {
+
+    private AddToCartHttpEndpointSync addToCartHttpEndpointSync;
+
     public enum UseCaseResult {
         SUCCESS,
         FAILURE,
         NETWORK_ERROR;
     }
+
+    AddToCartUseCaseSync(AddToCartHttpEndpointSync addToCartHttpEndpointSync) {
+        this.addToCartHttpEndpointSync = addToCartHttpEndpointSync;
+
+    }
+
+    public void addToCartSync(String offerId, int amount) {
+    }
+
 }
--- app/src/test/java/com/example/testdrivendevelopment_sample/exampleTwo/AddToCartUseCaseSyncTest.java
+++ app/src/test/java/com/example/testdrivendevelopment_sample/exampleTwo/AddToCartUseCaseSyncTest.java
@@ -1,9 +1,17 @@
 package com.example.testdrivendevelopment_sample.exampleTwo;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+import com.example.testdrivendevelopment_sample.exampleTwo.networking.AddToCartHttpEndpointSync;
+import com.example.testdrivendevelopment_sample.exampleTwo.networking.CartItemScheme;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 
@@ -12,8 +20,27 @@ public class AddToCartUseCaseSyncTest {
 
     private AddToCartUseCaseSync SUT;
 
+    private static final String OFFER_ID = "offerId";
+    private static final int AMOUNT = 4;
+
+    @Mock
+    private AddToCartHttpEndpointSync addToCartHttpEndpointSyncMock;
+
+
     @Before
     public void setUp() throws Exception {
-        SUT = new AddToCartUseCaseSync();
+        SUT = new AddToCartUseCaseSync(addToCartHttpEndpointSyncMock);
+
+
+    }
+
+    @Test
+    public void addToCartSync_correctParametersPassedToEndpoint() throws Exception{
+        ArgumentCaptor<CartItemScheme> ac = ArgumentCaptor.forClass(CartItemScheme.class);
+
+        SUT.addToCartSync(OFFER_ID, AMOUNT);
+        verify(addToCartHttpEndpointSyncMock).addToCartSync(ac.capture());
+        assertThat(ac.getValue().getOfferId(), is(OFFER_ID));
+        assertThat(ac.getValue().getAmount(), is(AMOUNT));
     }
 }
